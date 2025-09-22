0c3e8d82641c9694ff9ca8d59aa0fbb78d8c012f
SamuelSvenM <mardjuadisven@gmail.com>
2025-09-22 12:51:13 +1000

    Update tween

--- Assets/Scripts/Tween.cs
+++ Assets/Scripts/Tween.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tween : MonoBehaviour
+public class Tween
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform Target;
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+    public float ElapsedTime;
+    public float Duration;
+    public Tween(Transform target, Vector3 start, Vector3 end, float duration)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+        Target = target;
+        StartPosition = start;
+        EndPosition = end;
+        Duration = duration;
+        ElapsedTime = 0f;
+
+    }
+    public float Progress
     {
-        
+        get { return Mathf.Clamp01(ElapsedTime / Duration); }
     }
 }
+
+
+
+
+
+  
